7707c1e8150a9dbd524deb9cbfdfa45833d3ab30
viet-vo-codeleap <viet.vo@codeleap.de>
2026-03-16 10:28:57 +0700

    STRAND-0: adjust api to trigger pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! My name is Viet Vo from Code Leap. I am DevOps Engineer';
   }
 }
